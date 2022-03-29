74d476265fc6206b320505d48f8e5f3f727ec88e
Eric Swanson <85689409+eswanson-usgs@users.noreply.github.com>
2022-03-29 15:05:42 -0400

    Changed SofVwave3 to SofVwaveI

--- videoCurrentGen.m
+++ videoCurrentGen.m
@@ -135,7 +135,7 @@ for wind = 0:(Nb-1)
   % fit the S(v) to the model [Vamp Vmu Vsigma offset]
   beta0 = [maxv v(maxvind) 0.25 nanmean(V)];% nanmedian(V) 5];
   jv = v(gind);
-  vFun = @(beta0,jv) SofVwave3(beta0,jv,fkny);
+  vFun = @(beta0,jv) SofVwaveI(beta0,jv,fkny);
   [beta,resnorm,resid,eflag,outputs,lambda,jacb] = lsqcurvefit(vFun,double(beta0),jv,double(V(gind)),LB,UB,opts);
   fitted = SofVwaveI(beta,v(gind),fkny);
   dataStruct.meanV(j) = beta(2);
